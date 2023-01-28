325946d8fd4ee85680539f01367f52afb392b427
ChengHung-Wang <apple1314520iphone@icloud.com>
2023-01-29 00:51:30 +0800

    icons

---
+++ src/components/Home/AppItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home-app-item">
+    <img :src="require('/src/assets/appIcons/course_table.png')" alt="">
+    <p>課程</p>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "App"
+})
+</script>
+
+<style scoped>
+  .home-app-item {
+
+  }
+</style>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "UserCard"
+}
+</script>
+
+<style scoped>
+
+</style>
