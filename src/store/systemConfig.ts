a99746879bba09edf0c04efcbdbfa2ef5847c0ca
王正宏 <apple1314520iphone@icloud.com>
2023-02-19 05:01:22 +0800

    fix negative number time

--- src/store/systemConfig.ts
+++ src/store/systemConfig.ts
@@ -26,13 +26,13 @@ export const useSystemConfigStore = defineStore("systemConfig", {
     async getNextEvent() {
       const result = await request("GET", "/system/map/time/registration");
       if(result.status != 200) return false;
-      const today = new Date();
+      const today = moment();
       for(let timeDate of result.res.data) {
-        const startDate = new Date(timeDate.start_at);
-        const endDate = new Date(timeDate.end_at);
+        const startDate = moment(timeDate.start_at);
+        const endDate = moment(timeDate.end_at);
         if(today > endDate) continue;
         this.nextEvent = timeDate;
-        this.nextEvent.timeRange = [startDate, endDate];
+        this.nextEvent.timeRange = [startDate.toDate(), endDate.toDate()];
         if(today>startDate) this.nextEvent.status = "結束", this.nextEvent.closestTime = moment(timeDate.end_at);
         else this.nextEvent.status = "開始", this.nextEvent.closestTime = moment(timeDate.start_at);
         return this.nextEvent;
